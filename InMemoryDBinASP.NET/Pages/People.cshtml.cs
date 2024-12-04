06ec7bc86b845ceb71b819698beb824255d4d18c
Kaniz Fatema Shammy <kanizfatemakaniz3@gmail.com>
2024-12-04 16:43:57 +0600

    Razor Page added

---
+++ InMemoryDBinASP.NET/Pages/People.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InMemoryDBinASP.NET.Pages
+{
+    public class PeopleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
