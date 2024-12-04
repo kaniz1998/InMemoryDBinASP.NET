51915f9b5ba701ed97cbc29643c34bbe90d9bf0b
Kaniz Fatema Shammy <kanizfatemakaniz3@gmail.com>
2024-12-04 18:45:30 +0600

    Project Completed

--- InMemoryDBinASP.NET/Pages/People.cshtml.cs
+++ InMemoryDBinASP.NET/Pages/People.cshtml.cs
@@ -1,3 +1,4 @@
+using InMemoryDBinASP.NET.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +6,32 @@ namespace InMemoryDBinASP.NET.Pages
 {
     public class PeopleModel : PageModel
     {
+        private readonly MyDbContext _context;
+
+        public List<Person> People { get; set; } = new List<Person>();
+
+        [BindProperty]
+        public Person NewPerson { get; set; }
+
+        public PeopleModel(MyDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
-            
+            People = _context.People.ToList();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            _context.People.Add(NewPerson);
+
+            _context.SaveChanges();
+
+
+            return RedirectToPage();
         }
     }
 }
